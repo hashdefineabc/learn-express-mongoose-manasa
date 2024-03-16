01b7f273540bb7630da9bbf67bf93610ec2a7b8a
manasamanjunath <m.m@northeastern.edu>
2024-03-15 23:23:59 -0400

    03122024

--- pages/books_status.js
+++ pages/books_status.js
@@ -1,5 +1,30 @@
 let BookInstance = require('../models/bookinstance');
 
-exports.show_all_books_status = function(res) {
-  return res.send([]);
+function get_book(id) {
+  if (typeof id !== "string") {
+      return ({status: "error"});
+  }
+  return Book.findOne({'_id': {$eq: id}}).populate('author');
+}
+
+function get_book_dtl(id) {
+  return BookInstance
+          .find({ 'book': id })
+          .select('imprint status');
+}
+
+exports.show_all_books_status = async function(res) {
+  //return res.send([]);
+  const results = await Promise.all([get_book(id).exec(), get_book_dtl(id).exec()])
+  try {
+    let book = await results[0];
+    let bookInstance = await results[1];
+    res.send({
+      title: book.title,
+      status: bookInstance.status,
+    });
+  }
+  catch(err) {
+    res.send(`Book ${id} not found`);
+  } 
 }
